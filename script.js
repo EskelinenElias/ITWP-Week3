29b459c217c558b517c5502a388e1d205d7ed35c
Elias Eskelinen <104145838+EskelinenElias@users.noreply.github.com>
2024-09-16 19:23:21 +0300

    Task 1 commit

--- script.js
+++ script.js
@@ -1,9 +1,61 @@
 
+async function getData() {
+  const address = "https://statfin.stat.fi/PxWeb/sq/4e244893-7761-4c4f-8e55-7a8d41d86eff"
+  try {
+    const response = await fetch(address);
+    if (!response.ok) {
+      throw new Error("Something went wrong while attempting to fetch data.")
+    }
+    const data = await response.json();
+    return data;
+    
+  } catch(error) {
+    console.log("Something else went wrong while attempting to fetch data: ", error)
+  }
+}
 
-const address = "https://statfin.stat.fi/PxWeb/sq/4e244893-7761-4c4f-8e55-7a8d41d86eff"
+function addRow(label, value) {
+  // create a new row
+  const tableData = document.getElementById('table-data'); 
+  const row = document.createElement('tr');
+  const labelCell = document.createElement('td');
+  const valueCell = document.createElement('td');
+  // populate row with data
+  labelCell.innerText = label; 
+  valueCell.innerText = value; 
+  // add the row to the table
+  row.appendChild(labelCell); 
+  row.appendChild(valueCell); 
+  tableData.appendChild(row); 
+}
 
-fetch(address)
-  .then(response => response.json())
-  .then(data => console.log(data));
+async function updateTable() {
+  console.log("Updating table...")
+  // fetch data
+  const data = await getData();
+  // update table
+  
+  if (data) {
+    const labels = data.dataset.dimension.Alue.category.label; 
+    const indices = data.dataset.dimension.Alue.category.index; 
+    const values = data.dataset.value; 
+    for (let key in labels) {
+      let label = labels[key]; 
+      let index = indices[key]; 
+      let value = values[index]; 
+      console.log(`Value ${value}`)
+      addRow(label, value); 
+    }
+  }
+  console.log(data)
+  console.log("Table updated.");
+  
+}
 
 
+updateTable();
+
+//console.log(data.dataset.dimension.Alue.category.label); 
+//data.dataset.dimension.Alue.category.label.forEach(label => {
+//  console.log(`Municipality: ${label}`)
+//});
